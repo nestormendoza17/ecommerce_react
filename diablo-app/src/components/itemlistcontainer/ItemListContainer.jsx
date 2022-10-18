bb394cb661d170727d3784cc078679fa910ec0bd
Néstor Carlos Mendoza <nestor.mendoza17@yahoo.com.ar>
2022-10-17 22:57:39 -0300

    count

--- diablo-app/src/components/itemlistcontainer/ItemListContainer.jsx
+++ diablo-app/src/components/itemlistcontainer/ItemListContainer.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Title from "../title/Title";
+import ItemCount from "../itemcount/ItemCount";
 
 export const ItemListContainer = ({texto}) => {
+
+    const onAdd = (quantity) => {
+        console.log(`compraste ${quantity} unidades`)
+    }
+
     return (
         <div>
             <Title greeting={texto}/>
+            <ItemCount initial={1} stock={5} onAdd={onAdd} />
 
         </div>
     )
