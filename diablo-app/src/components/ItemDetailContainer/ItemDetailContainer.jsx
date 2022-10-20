1b4478e3a3d385855192b125d09c78b285009520
Néstor Carlos Mendoza <nestor.mendoza17@yahoo.com.ar>
2022-10-20 02:27:40 -0300

    firebase

--- diablo-app/src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ diablo-app/src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,14 +1,8 @@
 import React , {useState,useEffect}from 'react';
+import {getFirestore, doc, getDoc} from 'firebase/firestore';
 import ItemDetail from '../ItemDetail/ItemDetail';
 import {useParams} from 'react-router-dom';
 
-const vinos = [
-    {id: 1, image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/367/826/products/1391-d7d023796f9eb5ad4c16046855094017-640-0.jpg", title: "Vasija Secreta", category: "suave", price: 980,},
-    {id: 2, image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/367/826/products/assoy1-353865a255cc55964716104650980088-640-0.jpg", title: "Así como soy",category: "suave",price: 560,},
-    {id: 3, image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/367/826/products/sottano-malbecrose_11-dd64be96dc05aac3b016599925901330-320-0.jpg", title: "Sottano Rose",category: "suave",price: 670,},
-    {id: 4, image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/367/826/products/111-9e706d270f647a723216064095001609-320-0.jpg", title: "Tierra Prometida",category: "tinto",price: 700,},
-    {id: 5, image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/367/826/products/1281-fc6a1f3a834b9a124e16046839907053-320-0.jpg", title: "Gata Flora Pink",category: "tinto",price: 9000,},
-];
 
 const ItemDetailContainer = ()=> {
 
@@ -16,13 +10,11 @@ const ItemDetailContainer = ()=> {
     const {detalleId} = useParams();
 
     useEffect(()=> {
-        const getData = new Promise (resolve => {
-            setTimeout(()=> {
-                resolve(vinos);
-            }, 3000);
-        });
-        getData.then(res=> setData(res.find(vino=> vino.id === parseInt(detalleId))));
-    },[])
+        const querydb = getFirestore();
+        const queryDoc = doc(querydb, 'products', detalleId);
+        getDoc(queryDoc)
+        .then(res => setData({id: res.id, ...res.data()}))
+            },[detalleId])
     return (
         <ItemDetail data={data}/>
         
--- diablo-app/src/components/itemlistcontainer/ItemListContainer.jsx
+++ diablo-app/src/components/itemlistcontainer/ItemListContainer.jsx
@@ -1,32 +1,29 @@
 import "./itemListContainer.css";
 import React, {useState, useEffect} from "react";
+import {getFirestore, collection, getDocs, query, where} from "firebase/firestore";
 import Title from "../title/Title";
 import ItemList from "../ItemList/ItemList";
 import {useParams} from 'react-router-dom';
 
-const vinos = [
-    {id: 1, image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/367/826/products/1391-d7d023796f9eb5ad4c16046855094017-640-0.jpg", title: "Vasija Secreta", category: "suave", price: 980,},
-    {id: 2, image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/367/826/products/assoy1-353865a255cc55964716104650980088-640-0.jpg", title: "Así como soy",category: "suave",price: 560,},
-    {id: 3, image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/367/826/products/sottano-malbecrose_11-dd64be96dc05aac3b016599925901330-320-0.jpg", title: "Sottano Rose",category: "suave",price: 670,},
-    {id: 4, image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/367/826/products/111-9e706d270f647a723216064095001609-320-0.jpg", title: "Tierra Prometida",category: "tinto",price: 700,},
-    {id: 5, image: "https://d2r9epyceweg5n.cloudfront.net/stores/001/367/826/products/1281-fc6a1f3a834b9a124e16046839907053-320-0.jpg", title: "Gata Flora Pink",category: "tinto",price: 9000,},
-];
 
 export const ItemListContainer = ({texto}) => {
     const [data,setData] =useState([]);
     const {categoriaId} = useParams();
 
     useEffect(()=> {
-        const getData = new Promise(resolve => {
-            setTimeout(()=>{
-            resolve(vinos);
-            }, 1000)
-        });
-        if (categoriaId) {
+        const querydb = getFirestore();
+        const queryCollection = collection(querydb, 'products');
+        const queryFilter = query(queryCollection, where('category', '==', categoriaId), )
+        getDocs(queryFilter)
+        .then(res=> setData(res.docs.map(product => ({id:product.id, ...res.data()}))))
+    } else {
+        getDocs(queryCollection)
+        .then(res=> setData(res.docs.map(product=>({id:product.id, ...res.data()}))))
+        /* if (categoriaId) {
             getData.then(res=> setData(res.filter(vino=> vino.category === categoriaId)));
         } else {
         getData.then(res=> setData(res));
-    }
+    } */
     }, [categoriaId])
 
     
